091f217492abe9d7a767904e53fc6034bf510c57
Apple <apple@MacBook-Pro-Apple.local>
2024-07-14 18:43:35 +0300

    Добавлены lazy + Suspense

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,50 @@
-import { About } from "../pages/About"
-import { Favorites } from "../pages/Favorites"
-import { HistoryPage } from "../pages/HistoryPage"
-import { ItemPage } from "../pages/ItemPage"
-import { SignIn } from "../pages/SignIn"
-import { Signup } from "../pages/SignUp"
+import { lazy } from "react"
 
 export const publicRoutes = [
-  { path: "/signin", component: SignIn },
-  { path: "/signup", component: Signup },
-  { path: "/", component: About, exact: true },
-  { path: "/books/:id", component: ItemPage },
+  {
+    path: "/signin",
+    component: lazy(() =>
+      import("../pages/SignIn").then(module => ({ default: module.SignIn })),
+    ),
+  },
+  {
+    path: "/signup",
+    component: lazy(() =>
+      import("../pages/SignUp").then(module => ({ default: module.Signup })),
+    ),
+  },
+  {
+    path: "/",
+    component: lazy(() =>
+      import("../pages/About").then(module => ({ default: module.About })),
+    ),
+    exact: true,
+  },
+  {
+    path: "/books/:id",
+    component: lazy(() =>
+      import("../pages/ItemPage").then(module => ({
+        default: module.ItemPage,
+      })),
+    ),
+  },
 ]
 
 export const privateRoutes = [
-  { path: "/favorites", component: Favorites },
-  { path: "/history", component: HistoryPage },
+  {
+    path: "/favorites",
+    component: lazy(() =>
+      import("../pages/Favorites").then(module => ({
+        default: module.Favorites,
+      })),
+    ),
+  },
+  {
+    path: "/history",
+    component: lazy(() =>
+      import("../pages/HistoryPage").then(module => ({
+        default: module.HistoryPage,
+      })),
+    ),
+  },
 ]
