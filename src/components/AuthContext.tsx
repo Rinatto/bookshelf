581358a9843d0d8d3f15837ee2c64bf52c562a06
Apple <apple@MacBook-Pro-Apple.local>
2024-07-18 13:43:20 +0300

    Обернул login, logout и checkUser в useCallback

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react"
 import type React from "react"
-import { createContext, useEffect, useMemo, useState } from "react"
+import { createContext, useCallback, useEffect, useMemo, useState } from "react"
 
 interface AuthContextType {
   isAuth: boolean
@@ -26,28 +26,28 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
     null,
   )
 
-  const login = (user: { email: string; password: string }) => {
+  const login = useCallback((user: { email: string; password: string }) => {
     setIsAuth(true)
     setUser(user)
     localStorage.setItem("isAuth", "true")
     localStorage.setItem("currentUser", JSON.stringify(user))
-  }
+  }, [])
 
-  const logout = () => {
+  const logout = useCallback(() => {
     setIsAuth(false)
     setUser(null)
     localStorage.removeItem("isAuth")
     localStorage.removeItem("currentUser")
-  }
+  }, [])
 
-  const checkUser = (email: string, password: string) => {
+  const checkUser = useCallback((email: string, password: string) => {
     const users = JSON.parse(localStorage.getItem("users") || "[]")
     const foundUser = users.find(
       (u: { email: string; password: string }) =>
         u.email === email && u.password === password,
     )
     return !!foundUser
-  }
+  }, [])
 
   useEffect(() => {
     const auth = localStorage.getItem("isAuth")
@@ -60,7 +60,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
 
   const contextValue = useMemo(
     () => ({ isAuth, user, login, logout, checkUser }),
-    [isAuth, user],
+    [isAuth, user, login, logout, checkUser],
   )
 
   return (
